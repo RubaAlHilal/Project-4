62bff43fe7267aba5296d91a7eadad2fb7f01a04
Ali Hussain Arafat <alihma188@gmail.com>
2023-11-03 17:20:37 +0300

    watch globad data and models

---
+++ project_4/lib/data/watch_data_set.dart
@@ -0,0 +1,26 @@
+List<Map> watches = [
+  {
+    "name": "Zeitwerk Date",
+    "description": "It is a long established fact that a reader will be.",
+    "price": 3050.00,
+    "image": "assets/images/my_order_watch1.png",
+  },
+  {
+    "name": "Chronograph Radio",
+    "description": "It is a long established fact that a reader will be.",
+    "price": 3100.00,
+    "image": "assets/images/my_order_watch2.png",
+  },
+  {
+    "name": "Eco-Drive Baracelet",
+    "description": "It is a long established fact that a reader will be.",
+    "price": 4699.00,
+    "image": "assets/images/my_order_watch3.png",
+  },
+  {
+    "name": "Tourbillion Gold",
+    "description": "It is a long established fact that a reader will be.",
+    "price": 4250.00,
+    "image": "assets/images/my_order_watch4.png",
+  },
+];
