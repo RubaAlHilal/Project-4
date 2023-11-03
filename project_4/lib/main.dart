8fecf3ebc9f6512ba7955dd51696b0663982be10
Ali Hussain Arafat <alihma188@gmail.com>
2023-11-03 19:39:48 +0300

    merge ruba work, and custom button

--- project_4/lib/main.dart
+++ project_4/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project_4/screens/HomePage/homepage_screen.dart';
+import 'package:project_4/screens/ProductDetailsPage/product_details_screen.dart';
+import 'package:project_4/screens/SearchPage/search_screen.dart';
 import 'package:project_4/screens/start_screen.dart';
 
 import 'screens/filter/filter_screen.dart';
@@ -14,7 +17,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StartScreen(),
+      home: ProductDetailsScreen(),
     );
   }
 }
